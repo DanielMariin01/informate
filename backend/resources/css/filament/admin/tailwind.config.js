97304fb90a0633815d20dffec851dcdfac009acf
DanielMariin01 <danielstivenmarin0@gmail.com>
2025-04-28 08:14:21 -0500

    segundo commit

---
+++ backend/resources/css/filament/admin/tailwind.config.js
@@ -0,0 +1,13 @@
+// resources/css/filament/admin/tailwind.config.js
+
+import colors from 'tailwindcss/colors'
+
+export default {
+    theme: {
+        extend: {
+            colors: {
+                primary: colors.blue, // Esto hará que el color principal sea azul
+            },
+        },
+    },
+}
